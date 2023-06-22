c14d7e090dfe1afbe80195c2654693f397579edf
leolorenzoluis <leolorenzoluis@outlook.com>
2023-06-22 16:23:12 -0400

    test

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,56 +15,111 @@ const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
   const json = await req.json()
-  const { messages, previewToken } = json
-  const session = await auth()
+  const { messages } = json
+  const data = {
+    question: messages[messages.length - 1].content,
+    chat_history: messages.slice(0, messages.length - 1),
+  };
 
-  if (process.env.VERCEL_ENV === 'preview') {
-    if (session == null) {
-      return new Response('Unauthorized', { status: 401 })
-    }
+  const body = JSON.stringify(data);
+  console.log('data', data);
+
+  const url = 'https://finalprompt-fwsoq.eastus.inference.ml.azure.com/score';
+  // Replace this with the primary/secondary key or AMLToken for the endpoint
+
+  const api_key = process.env.API_KEY;
+  console.log('API KEY', api_key)
+  if (!api_key) {
+    throw new Error('A key should be provided to invoke the endpoint');
   }
 
-  if (previewToken) {
-    configuration.apiKey = previewToken
-  }
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${api_key}`,
+    'azureml-model-deployment': 'blue',
+  };
 
-  const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
+  const options = {
+    method: 'POST',
+    headers: headers,
+    body: body,
+    transferEncoding: 'chunked',
+  };
 
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const userId = session?.user?.id
-      if (userId) {
-        const id = json.id ?? nanoid()
-        const createdAt = Date.now()
-        const path = `/chat/${id}`
-        const payload = {
-          id,
-          title,
-          userId,
-          createdAt,
-          path,
-          messages: [
-            ...messages,
-            {
-              content: completion,
-              role: 'assistant'
-            }
-          ]
+  const fetchResponse = await fetch(url, options);
+  const message = await fetchResponse.json();
+  const botAnswer = message['chat_output'];
+  // Create a readable stream from botAnswer
+
+
+  // console.log('fetchResponse', await fetchResponse.json())
+  const botAnswerChunks = botAnswer.match(/.{1,10}/g) || [];
+  let i = 0;
+  const readableStream = new ReadableStream({
+    start(controller) {
+      console.log('heyyy');
+      let i = 0;
+      const intervalId = setInterval(() => {
+        if (i >= botAnswerChunks.length) {
+          clearInterval(intervalId);
+          controller.close();
+          return;
         }
-        await kv.hmset(`chat:${id}`, payload)
-        await kv.zadd(`user:chat:${userId}`, {
-          score: createdAt,
-          member: `chat:${id}`
-        })
-      }
-    }
-  })
 
-  return new StreamingTextResponse(stream)
+        const chunk = botAnswerChunks[i];
+        const encoder = new TextEncoder();
+        const chunkAsUint8Array = encoder.encode(chunk);
+
+        controller.enqueue(chunkAsUint8Array);
+        i++;
+      }, 10);
+    },
+  });
+  return new StreamingTextResponse(readableStream);
+
+  // Create a new write stream
+
+  // if (previewToken) {
+  //   configuration.apiKey = previewToken
+  // }
+
+  // const res = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   messages,
+  //   temperature: 0.7,
+  //   stream: true
+  // })
+
+  // const stream = OpenAIStream(res, {
+  //   async onCompletion(completion) {
+  //     const title = json.messages[0].content.substring(0, 100)
+  //     const userId = session?.user?.id
+  //     if (userId) {
+  //       const id = json.id ?? nanoid()
+  //       const createdAt = Date.now()
+  //       const path = `/chat/${id}`
+  //       const payload = {
+  //         id,
+  //         title,
+  //         userId,
+  //         createdAt,
+  //         path,
+  //         messages: [
+  //           ...messages,
+  //           {
+  //             content: completion,
+  //             role: 'assistant'
+  //           }
+  //         ]
+  //       }
+  //       await kv.hmset(`chat:${id}`, payload)
+  //       await kv.zadd(`user:chat:${userId}`, {
+  //         score: createdAt,
+  //         member: `chat:${id}`
+  //       })
+  //     }
+  //   }
+  // })
+
+  // return new StreamingTextResponse(stream)
 }
