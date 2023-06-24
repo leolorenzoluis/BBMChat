77a76fcb801a3f9ec7a35b4ca57ac3fcf7c1847d
leolorenzoluis <leolorenzoluis@outlook.com>
2023-06-24 03:47:49 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true,
+    serverActions: true
   },
-};
+  webpack: (config, options) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.node/,
+      use: 'raw-loader'
+    })
+    return config
+  }
+}
