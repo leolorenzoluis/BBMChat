22494f9cd11ed6b81a26c1f84b8966926405b5dc
leolorenzoluis <leolorenzoluis@outlook.com>
2023-06-25 08:19:04 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
 
+import { Analytics } from '@vercel/analytics/react'
 export const runtime = 'edge'
 
 export default function IndexPage() {
   const id = nanoid()
 
-  return <Chat id={id} />
+  return (
+    <>
+      <Analytics />
+      <Chat id={id} />
+    </>
+  )
 }
